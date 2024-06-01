be53b50be66901ddbfcf87d5543935441174e910
Wiktoria Płonka <wiktoria.plonka@polsl.pl>
2024-06-01 03:04:35 +0200

    HW1 v1

---
+++ README-NEW.md
@@ -0,0 +1,5 @@
+**Name:** Wiktoria Płonka
+**email:** wiktoria.plonka@polsl.pl
+**GitHub ID:** wplonka
+
+![My image](CCf_sim_CCF_m_v3.png)
--- hello.py
+++ hello.py
@@ -1,2 +1,6 @@
 def hello_world():
-	return "Hello"
+	return "Hello World!"
+
+
+def hello_world_n(N):
+    return " ".join("Hello World!" * N)
